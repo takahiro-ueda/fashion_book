0e4e2725505a083dfea58e33a03508395001f9c7
takahiro-ueda <pt25ue@gmail.com>
2020-06-15 18:53:12 +0900

    アイテム削除　リンク変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.destroy
-      redirect_to root_path, notice: '削除に成功しました。'
+      redirect_to items_path, notice: '削除に成功しました。'
     else
       flash.now[:alert] = '削除に失敗しました。'
       render :edit
